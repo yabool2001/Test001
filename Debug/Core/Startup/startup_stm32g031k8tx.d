518147c04700058e40e938687c305e3f38943dce
yabool2001 <mzemlo.pl@gmail.com>
2022-08-13 08:43:58 +0200

    aa

--- Debug/Core/Startup/startup_stm32g031k8tx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32g031k8tx.o: \
- ../Core/Startup/startup_stm32g031k8tx.s
--- Debug/Drivers/IIS2DLPC/iis2dlpc_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/IIS2DLPC/iis2dlpc_reg.o: ../Drivers/IIS2DLPC/iis2dlpc_reg.c \
- ../Drivers/IIS2DLPC/iis2dlpc_reg.h
-
-../Drivers/IIS2DLPC/iis2dlpc_reg.h:
